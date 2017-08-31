03390fad842823b7b3424ef408573b5379fa8040
j129008 <j129008@gmail.com>
2017-09-01 02:38:43 +0800

    autodump

--- trader.py
+++ trader.py
@@ -18,18 +18,20 @@ class future(Thread):
         resfut.encoding = 'utf-8'
         soup = BS(resfut.text,"lxml")
         table = pd.read_html(str(soup.select('#divDG')[0]),index_col=0,header=0)[0]
-        return table.filter(regex='^小臺指期0|^小臺指期1', axis=0).iloc[0:1]
+        price = table.filter(regex='^小臺指期0|^小臺指期1', axis=0).iloc[0:1]
+        print( price )
+        return price
 
     def update_price(self):
         self.price_set = pd.concat([self.price_set, self.get_price()])
 
     def run(self):
         current_time = datetime.strftime(datetime.now(),"%H:%M:%S")
-        end_time = "13:45:00"
+        end_time = "13:45:15"
         while current_time < end_time:
             self.update_price()
             time.sleep(15)
-        pickle.dump(self.price_set, open('./history/' + datetime.strftime(datetime.now(),"%Y_%m_%d") + '.pkl', 'wb'))
+            pickle.dump(self.price_set, open('./history/' + datetime.strftime(datetime.now(),"%Y_%m_%d") + '.pkl', 'wb'))
 
 if __name__ == "__main__":
     fut = future()
