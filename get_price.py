0ca8705a1085f8c86857a345e1e17aa4a7d8ba44
j129008 <j129008@gmail.com>
2017-08-31 15:44:50 +0800

    wrap with class

--- get_price.py
+++ get_price.py
@@ -2,18 +2,18 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup as BS
 
-def get_price():
-    urlfut = 'http://info512.taifex.com.tw/Future/FusaQuote_Norl.aspx?_Category=1'
-    resfut = requests.get(urlfut)
-    resfut.encoding = 'utf-8'
-    soup = BS(resfut.text,"lxml")
-    table = pd.read_html(str(soup.select('#divDG')[0]),index_col=0,header=0)[0]
-    return table.filter(regex='^小臺指期0|^小臺指期1', axis=0).iloc[0:1]
+class future:
+    def __init__(self):
+        self.price_set = self.get_price()
 
-def update_price(data_set):
-    return pd.concat([data_set, get_price()])
+    def get_price(self):
+        urlfut = 'http://info512.taifex.com.tw/Future/FusaQuote_Norl.aspx?_Category=1'
+        resfut = requests.get(urlfut)
+        resfut.encoding = 'utf-8'
+        soup = BS(resfut.text,"lxml")
+        table = pd.read_html(str(soup.select('#divDG')[0]),index_col=0,header=0)[0]
+        return table.filter(regex='^小臺指期0|^小臺指期1', axis=0).iloc[0:1]
 
-# init data_set
-data_set = get_price()
+    def update_price(self):
+        self.price_set = pd.concat([self.price_set, self.get_price()])
 
-data_set = update_price(data_set)
