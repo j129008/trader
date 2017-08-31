2ceda7d7ae43d8cbb58f8ea7ba7fb16bd02aaff5
j129008 <j129008@gmail.com>
2017-09-01 03:44:35 +0800

    crontab

--- time_for_work.py
+++ time_for_work.py
@@ -1,3 +1,9 @@
 from crontab import CronTab
+from datetime import time
 
 trader_cron = CronTab(user=True)
+job = trader_cron.new(command='/usr/bin/python3 /home/vodo/trader/trader.py')
+job.setall('45 8 * * 1-5')
+job.set_comment('time to trade')
+
+trader_cron.write()
