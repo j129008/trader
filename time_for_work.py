a59c10147b355dea1746c85b2bf384d3c5259163
j129008 <j129008@gmail.com>
2017-09-01 15:58:35 +0800

    put err log

--- time_for_work.py
+++ time_for_work.py
@@ -2,7 +2,7 @@ from crontab import CronTab
 from datetime import time
 
 trader_cron = CronTab(user=True)
-job = trader_cron.new(command='/usr/bin/python3 /home/vodo/trader/trader.py')
+job = trader_cron.new(command='/usr/bin/python3 /home/vodo/trader/trader.py > /tmp/cron.log 2>&1')
 job.setall('45 8 * * 1-5')
 job.set_comment('time to trade')
 
