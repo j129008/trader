107c8de536f0f0cdfb328d502c8cc0a9957e0f34
j129008 <j129008@gmail.com>
2017-09-02 18:21:33 +0800

    check file exist

--- data_downloader.py
+++ data_downloader.py
@@ -1,18 +1,26 @@
 from datetime import date, timedelta
 import urllib.request
+from pathlib import Path
 
 class fut_data:
     def __init__(self, data_location):
         self.url = 'http://www.taifex.com.tw/DailyDownload/DailyDownloadCSV/Daily_'
         self.today = date.today()
         self.folder = data_location
-    def get_today(self):
-        file_name = self.today.strftime('%Y_%0m_%0d') + '.zip'
-        if self.today.isoweekday() not in [6, 7]:
-            urllib.request.urlretrieve(self.url + file_name, self.folder + file_name)
+    def past_day(self, day):
+        past_day = self.today - timedelta(days=day)
+        file_name = past_day.strftime('%Y_%0m_%0d') + '.zip'
+        file_path = Path(self.folder + file_name)
+        if not file_path.exists():
+            if past_day.isoweekday() not in [6, 7]:
+                urllib.request.urlretrieve(self.url + file_name, self.folder + file_name)
+                print('save ' + file_name + ' success')
+            else:
+                print('is weekend')
         else:
-            print('is weekend')
+            print('file exist')
 
 if __name__ == '__main__':
     fut = fut_data('/home/vodo/trader/history/')
-    fut.get_today()
+    for i in range(30):
+        fut.past_day(i)
